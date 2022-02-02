7a49064721dd99c78bbb42a80736166ce844028c
Natalia <atvnataliajn@gmail.com>
2022-02-03 00:35:43 +0100

    modify fetch url to localhost

--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,8 +1,10 @@
 const postToApi = (userData) => {
-	return fetch("https://awesome-profile-cards.herokuapp.com/card", {
-		method: "POST",
-		body: JSON.stringify(userData),
-		headers: { "Content-Type": "application/json" },
-	}).then((response) => response.json());
+  return fetch("https://localhost:4000/card/:cardId", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log("Error:", error));
 };
 export default postToApi;
