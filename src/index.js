a78b947520fc87b80400b2202ad9f575a1ebd5c8
IsaGarabana <isinhagb@gmail.com>
2022-02-07 12:53:43 +0100

    add styles

--- src/index.js
+++ src/index.js
@@ -8,51 +8,53 @@ const server = express();
 // Configuramos el servidor
 server.use(cors());
 server.use(
-  express.json({
-    limit: "5mb",
-  })
+	express.json({
+		limit: "5mb",
+	})
 );
 server.set("view engine", "ejs");
 
 // Arrancamos el servidor en el puerto 4000
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+	console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
 // Escribimos los endpoints que queramos
 server.post("/card", (req, res) => {
-  const responseSuccess = {
-    success: true,
-    cardURL: "//localhost:4000/card/1323456",
-  };
+	const responseSuccess = {
+		success: true,
+		cardURL: "//localhost:4000/card/1323456",
+	};
 
-  const responseError = {
-    success: false,
-    cardURL: null,
-  };
+	const responseError = {
+		success: false,
+		cardURL: null,
+	};
 
-  if (
-    req.body.name !== "" &&
-    req.body.job !== "" &&
-    req.body.email !== "" &&
-    req.body.linkedin !== "" &&
-    req.body.github !== "" &&
-    req.body.photo !== ""
-  ) {
-    res.json(responseSuccess);
-  } else {
-    res.json(responseError);
-  }
+	if (
+		req.body.name !== "" &&
+		req.body.job !== "" &&
+		req.body.email !== "" &&
+		req.body.linkedin !== "" &&
+		req.body.github !== "" &&
+		req.body.photo !== ""
+	) {
+		res.json(responseSuccess);
+	} else {
+		res.json(responseError);
+	}
 });
 
 server.get("/card/:cardId", (req, res) => {
-  res.render("card");
+	res.render("card");
 });
-const staticServerStyles = "./web/src/styles/";
-server.use(express.static(staticServerStyles));
 
 // servidor de estáticos
+
+//Servidor de CSS
+const staticServerStyles = "./src/styles";
+server.use(express.static(staticServerStyles));
 const staticServerPath = "./src/public-react"; // ruta de la carpeta donde vamos a guardar todos los ficheros estáticos
 server.use(express.static(staticServerPath)); // le decimos al servidor que use el servidor estático de express
 
